bebb4b19cec665e884097e27460db5a1c655a332
BamoJ <94501520+BamoJ@users.noreply.github.com>
2023-08-26 10:43:00 +0700

    5615614561

--- se-01-three.js
+++ se-01-three.js
@@ -14,12 +14,15 @@ const seed = random.getRandomSeed()
 const settings = {
 	// Make the loop animated
 	animate: true,
+	fps: 60,
+	dimensions: [1080, 1080],
+	duration: 4,
 	// Get a WebGL canvas rather than 2D
 	context: 'webgl',
 	// Turn on MSAA
 	attributes: {
 		antialias: true,
-		dpr: 2,
+		powerPreference: 'high-performance',
 	},
 	suffix: seed,
 }
@@ -34,38 +37,61 @@ const sketch = ({ context }) => {
 	renderer.setClearColor('#ffffff', 1)
 
 	// Setup a camera
-	const camera = new THREE.PerspectiveCamera(65, 1, 0.01, 100)
-	camera.position.set(4, 2, 2)
-	camera.lookAt(new THREE.Vector3())
+	const camera = new THREE.OrthographicCamera()
 
 	// Setup camera controller
-	const controls = new THREE.OrbitControls(camera, context.canvas)
+
+	// const controls = new THREE.OrbitControls(camera, context.canvas)
 
 	// Setup your scene
 	const scene = new THREE.Scene()
 
 	// Setup a geometry
-	const geometry = new THREE.BoxGeometry(1, 1, 1, 50, 50, 50)
+	const geometry = new THREE.BoxGeometry(1, 1, 1)
 
 	// Setup a material
-	const material = new THREE.MeshPhysicalMaterial({
-		color: 'white',
-		wireframe: false,
-		roughness: 0.75,
-		flatShading: true,
-	})
+
+	const palette = random.pick(palettes, seed)
+
+	const num = 30
+
+	for (let i = 0; i < num; i++) {
+		const material = new THREE.MeshPhongMaterial({
+			color: random.pick(palette),
+			flatShading: false,
+			reflectivity: 10,
+			shininess: 100,
+			refractionRatio: 0.85,
+		})
+		const mesh = new THREE.Mesh(geometry, material)
+
+		mesh.position.set(
+			random.range(-1, 1),
+			random.range(-1, 1),
+			random.range(-1, 1),
+		)
+		mesh.scale.set(
+			random.range(-0.5, 0.5),
+			random.range(-0.5, 0.5),
+			random.range(-0.5, 0.5),
+		),
+			scene.add(mesh)
+	}
 
 	// setup a light
-	const pLight = new THREE.PointLight('#45caf7', 1, 15.5)
-	pLight.position.set(2, 2, 4).multiplyScalar(1.5)
 
-	const aLight = new THREE.AmbientLight('skyblue', 0.3)
+	// Directional light
+	const dLight = new THREE.DirectionalLight('white', 1)
+	dLight.position.set(0, 4, Math.PI * 2)
 
-	scene.add(pLight, aLight)
+	// point light
+	// const pLight = new THREE.PointLight('#45caf7', 1, 15.5)
+	// pLight.position.set(2, 2, 4).multiplyScalar(1.5)
 
-	// Setup a mesh with geometry + material
-	const mesh = new THREE.Mesh(geometry, material)
-	scene.add(mesh)
+	// ambient light
+	const aLight = new THREE.AmbientLight('hsl(0, 0%, 20%)')
+
+	scene.add(dLight, aLight)
 
 	// draw each frame
 	return {
@@ -73,18 +99,41 @@ const sketch = ({ context }) => {
 		resize({ pixelRatio, viewportWidth, viewportHeight }) {
 			renderer.setPixelRatio(pixelRatio)
 			renderer.setSize(viewportWidth, viewportHeight, false)
-			camera.aspect = viewportWidth / viewportHeight
+
+			// aspect ratio
+			const aspect = viewportWidth / viewportHeight
+
+			// Orthographic camera zoom
+			const zoom = 1.9
+
+			// Camera Bounds
+			camera.left = -zoom * aspect
+			camera.right = zoom * aspect
+			camera.top = zoom
+			camera.bottom = -zoom
+
+			// Near/Far Camera
+			camera.near = -100
+			camera.far = 100
+
+			// Set camera position and look at scene
+			camera.position.set(zoom, zoom, zoom)
+			camera.lookAt(new THREE.Vector3())
+
+			// Update the camera
 			camera.updateProjectionMatrix()
 		},
+
 		// Update & render your scene here
 		render({ time }) {
-			controls.update()
+			// controls.update()
 			renderer.render(scene, camera)
-			mesh.rotation.y = time * ((10 * Math.PI) / 180)
+			scene.rotation.y = time * 0.1
 		},
+
 		// Dispose of events & renderer for cleaner hot-reloading
 		unload() {
-			controls.dispose()
+			// controls.dispose()
 			renderer.dispose()
 		},
 	}
